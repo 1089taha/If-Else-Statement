5ebafa2d17cfc984ad20b7bab3f3dd7ad883acf6
1089taha <161455916+1089taha@users.noreply.github.com>
2024-07-16 16:12:25 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+/*let theNumber = 10;
+if (10 % 2 == 0){
+    console.log('Even')
+}else{
+    console.log('Odd')
+}
+
+let score = 75;
+if (score >= 90){
+    console.log("Grade A")
+}else if (score >= 80 && score <= 89){
+    console.log("Grade B")
+}else if (score >= 70 && score <= 79){
+    console.log("Grade C")
+}else{
+    console.log("Grade D")
+}
+
+let age = 19;
+if (age >= 0 && age <= 12){
+    console.log("Child")
+}else if (age >= 13 && age <= 19){
+    console.log("Teenager")
+}else if (age >= 20){
+    console.log("adult")
+}else {
+    console.log("Invalid Age")
+}
+
+let traffic_light="red";
+if(traffic_light === "green"){
+    console.log("GO")
+}else if (traffic_light === "red"){
+    console.log("STOP")
+}else if (traffic_light === "yellow"){
+    console.log("GET READY")
+}else{
+    console.log("INVALID COLOR")
+}
+
+let weather = "snowy";
+if(weather === "sunny"){
+    console.log("Let's go to the beach")
+}else if (weather === "rainy"){
+    console.log( "Let's stay indoors and read a book")
+}else if (weather === "snowy"){
+    console.log( "Let's build a snowman")
+}else{
+    console.log("Let's just relax at home")
+}*/
+var score = 85;
+if (score >= 90) {
+    if (score === 87) {
+    }
+    console.log("Excellent Score");
+}
+else if (score >= 80 && score <= 89) {
+    console.log("Good Score");
+}
+else if (score >= 70 && score <= 79) {
+    console.log("Average Score");
+}
+else {
+    console.log("Bad Score");
+}
---
+++ index.ts
@@ -0,0 +1,65 @@
+let theNumber = 10;
+if (10 % 2 == 0){
+    console.log('Even')
+}else{
+    console.log('Odd')
+}
+
+let exam_score = 75;
+if (exam_score >= 90){
+    console.log("Grade A")
+}else if (exam_score >= 80 && score <= 89){
+    console.log("Grade B")
+}else if (exam_score >= 70 && score <= 79){
+    console.log("Grade C")
+}else{
+    console.log("Grade D")
+}
+
+let age = 19;
+if (age >= 0 && age <= 12){
+    console.log("Child")
+}else if (age >= 13 && age <= 19){
+    console.log("Teenager")
+}else if (age >= 20){
+    console.log("adult")
+}else {
+    console.log("Invalid Age")
+}
+
+let traffic_light="red";
+if(traffic_light === "green"){
+    console.log("GO")
+}else if (traffic_light === "red"){
+    console.log("STOP")
+}else if (traffic_light === "yellow"){
+    console.log("GET READY")
+}else{
+    console.log("INVALID COLOR")
+}
+
+let weather = "snowy";
+if(weather === "sunny"){
+    console.log("Let's go to the beach")
+}else if (weather === "rainy"){
+    console.log( "Let's stay indoors and read a book")
+}else if (weather === "snowy"){
+    console.log( "Let's build a snowman")
+}else{
+    console.log("Let's just relax at home")
+}
+let percentage = 85;
+if (percentage >= 90) {
+    if (percentage === 87) {
+    }
+    console.log("Excellent percentage");
+}
+else if (percentage >= 80 && score <= 89) {
+    console.log("Good percentage");
+}
+else if (percentage >= 70 && score <= 79) {
+    console.log("Average percentage");
+}
+else {
+    console.log("Bad percentage");
+}
